8b8b09d0fef6d027800196b2f4f0a9918a907b58
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2022-12-29 01:04:26 +0800

    Implement POST roster pokemon

---
+++ pages/api/roster/[id]/pokemon/index.ts
@@ -0,0 +1,38 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient, RosterPokemon } from '@prisma/client';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<RosterPokemon | RosterPokemon[] | string>
+) {
+  const { id } = req.query;
+
+  const roster = await prisma.roster.findUnique({
+    where: {
+      id: parseInt(id as string),
+    },
+  });
+
+  if (roster) {
+    if (req.method === 'POST') {
+      const { pokemonId } = req.body;
+
+      const rosterPokemon = await prisma.rosterPokemon.create({
+        data: {
+          rosterId: roster.id,
+          pokemonId,
+        },
+      });
+
+      res.status(201).json(rosterPokemon);
+    } else if (req.method === 'GET') {
+    } else {
+      res.status(400).send('Specify an id in the endpoint if making a PUT, or DELETE request');
+    }
+  } else {
+    res.status(400).send('Roster not found');
+  }
+}
